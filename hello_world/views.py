98cf02575d6516ad0f7211ebd822c86d78a04258
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-06-17 12:53:02 +0000

    get and post method req for index

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("wagwarn")
+    if request.method == "GET":
+        return HttpResponse("This was a GET request")
+    elif request.method == "POST":
+       return HttpResponse("This was a POST request")
